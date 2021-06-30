ca1661cb521a4560fa7aa3747d28757b5806031b
Hamza Khalid <hamzer95@gmail.com>
2021-06-29 21:46:58 -0400

    added timer to automatically check hour, day reset and loading from local storage. scheduler fully functioning to specifications

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,19 +31,61 @@ $(document).ready(function(){
 
     hourChecker();
 
-  //listen for save button clicks and save inputted values to LocalStorage
-  $(".saveBtn").on("click", function() {
+    //listen for save button clicks and save inputted values to LocalStorage
+    var events = []
+    $(".saveBtn").on("click", function() {
     
-    var value = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-    var dateAdded = moment().format("dddd, MMMM Do");
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        var dateAdded = moment().format("dddd, MMMM Do");
 
-    events.push({description: value, time: time, date: dateAdded});
+        events.push({description: value, time: time, date: dateAdded});
 
-    localStorage.setItem("events", JSON.stringify(events));
+        localStorage.setItem("events", JSON.stringify(events));
     
-  });
+    });
 
+    //calls hourChecker every 10 seconds to check if time block's classes
+    //need to be updated
+    var Refresh = 10;
+    function setTime() {
+    setInterval(function() {
+      Refresh--;
   
+      if(Refresh === 0) {
+        hourChecker();
+        Refresh = 10;
+      }
+  
+        }, 1000);
+    }
+
+    setTime();
+
+    // reset on new day
+    var currentDay = moment().format("dddd, MMMM Do");
+    for(var i = 0; i < events.length; i++) {
+        if(currentDay.isAfter(events[i].date)) {
+        events[i].description = "";
+            events[i].time = "";
+            events[i].date = "";
+            events.length = 0;
+        }
+    }
+  
+    // load saved data from localStorage
+    var storedEvents = JSON.parse(localStorage.getItem("events"));
+  
+    if (storedEvents !== null) {
+      events = storedEvents;
+    }
+  
+    for(var i = 0; i < events.length; i++) {
+      var userDescription = events[i].description;
+      $("#" + events[i].time).children(".description").text(userDescription);
+    }
+
+
+
     
 });
